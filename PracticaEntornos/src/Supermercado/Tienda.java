a86ec6c602f5e344510eeb50fb67e44f2cea93ac
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:10:11 +0100

    Creacion de la clase tienda

---
+++ PracticaEntornos/src/Supermercado/Tienda.java
@@ -0,0 +1,20 @@
+package Supermercado;
+
+import java.util.ArrayList;
+
+public class Tienda {
+
+	private String nombre;
+	private ArrayList<Producto> productos;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+	
+	
+	
+}
