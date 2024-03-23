c918c17dd689084764a8c67864c640b582249dd8
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:19:36 +0100

    metodos agregar y eliminar producto

--- PracticaEntornos/src/Supermercado/Tienda.java
+++ PracticaEntornos/src/Supermercado/Tienda.java
@@ -6,18 +6,24 @@ public class Tienda {
 
 	private String nombre;
 	private ArrayList<Producto> productos;
-	
+
 	public Tienda(String nombre) {
 		this.nombre = nombre;
 		this.productos = new ArrayList<Producto>();
 	}
+	public void agregarProducto(Producto producto) {
+		productos.add(producto);
+		System.out.println("Se ha añadido el producto a la tienda");
+	}
 	
+	public void eliminarProducto(Producto producto) {
+		productos.remove(producto);
+		System.out.println("Este producto se ha eliminado de la tienda");
+	}
+	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
-	
-	
-	
-	
+
 }
