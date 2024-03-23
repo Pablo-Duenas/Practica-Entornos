8c0990b2884f2ec97474a8ff54f18de7703698a6
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:50:02 +0100

    implementacion javadoc en la clase tienda

--- PracticaEntornos/src/Supermercado/Tienda.java
+++ PracticaEntornos/src/Supermercado/Tienda.java
@@ -2,26 +2,52 @@ package Supermercado;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author pablo
+ * @version 1.0
+ * @since 23-03-2024
+ */
 public class Tienda {
 
 	private String nombre;
 	private ArrayList<Producto> productos;
 
+	/**
+	 * Constructor de la tienda
+	 * 
+	 * @param nombre para saber el nombre de la tienda
+	 */
 	public Tienda(String nombre) {
 		this.nombre = nombre;
 		this.productos = new ArrayList<Producto>();
 	}
+
+	/**
+	 * Metodo para agregar un producto
+	 * 
+	 * @param producto El producto que quieres agregar
+	 */
 	public void agregarProducto(Producto producto) {
 		productos.add(producto);
 		System.out.println("Se ha añadido el producto a la tienda");
 	}
-	
+
+	/**
+	 * Metodo para eliminar un producto
+	 * 
+	 * @param producto El producto que quieres eliminar
+	 */
 	public void eliminarProducto(Producto producto) {
 		productos.remove(producto);
 		System.out.println("Este producto se ha eliminado de la tienda");
 	}
-	
 
+	/**
+	 * Metodo para saber el nombre de la tienda
+	 * 
+	 * @return El nombre de la tienda
+	 */
 	public String getNombre() {
 		return nombre;
 	}
