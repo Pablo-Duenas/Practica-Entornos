06b2593cba75970bdf48cb44e5fc425e58ddaab7
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 16:41:44 +0100

    Atributos getters and setters

--- PracticaEntornos/src/Supermercado/Producto.java
+++ PracticaEntornos/src/Supermercado/Producto.java
@@ -3,5 +3,31 @@ package Supermercado;
 public class Producto {
 
 	private String nombre;
-	
+	private double precio;
+	private boolean enStock;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public boolean isEnStock() {
+		return enStock;
+	}
+
+	public void setEnStock(boolean enStock) {
+		this.enStock = enStock;
+	}
+
 }
