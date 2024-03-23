effcc5e4150d42605ad012f3d2b4ff7517486894
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:37:08 +0100

    implementacion javadoc en la clase producto

--- PracticaEntornos/src/Supermercado/Producto.java
+++ PracticaEntornos/src/Supermercado/Producto.java
@@ -1,17 +1,37 @@
 package Supermercado;
 
+/**
+ * clase de un producto de una tienda
+ * 
+ * @author pablo
+ * @version 1.0
+ * @since 23-03-2024
+ * 
+ */
 public class Producto {
 
 	private String nombre;
 	private double precio;
 	private boolean enStock;
 
+	/**
+	 * constructor del producto
+	 * 
+	 * @param nombre  nombre del producto
+	 * @param precio  precio del producto
+	 * @param enStock si el producto esta o no en stock
+	 */
 	public Producto(String nombre, double precio, boolean enStock) {
 		this.nombre = nombre;
 		this.precio = precio;
 		this.enStock = enStock;
 	}
 
+	/**
+	 * Metodo para saber si esta o no en stock
+	 * 
+	 * @return devuelve true si el producto esta en stock y false si no lo esta
+	 */
 	public boolean estaEnStock() {
 		if (enStock) {
 			System.out.println("Este producto esta en Stock");
@@ -22,6 +42,12 @@ public class Producto {
 		}
 	}
 
+	/**
+	 * Metodo para cambiar el precio del producto
+	 * 
+	 * @param nuevoPrecio es el valor del nuevo precio del producto
+	 * @return devuelve el nuevo precio del producto
+	 */
 	public double cambiarPrecio(double nuevoPrecio) {
 		precio = nuevoPrecio;
 
@@ -29,19 +55,31 @@ public class Producto {
 
 	}
 
+	/**
+	 * Metodo del nombre
+	 * 
+	 * @return devuelve el nombre del producto
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
-
+	/**
+	 * Metodo del precio
+	 * 
+	 * @return devuelve el precio del producto
+	 */
 	public double getPrecio() {
 		return precio;
 	}
 
-
+	/**
+	 * Metodo de Stock
+	 * 
+	 * @return devuelve true o false si esta en stock o no
+	 */
 	public boolean isEnStock() {
 		return enStock;
 	}
-
 
 }
