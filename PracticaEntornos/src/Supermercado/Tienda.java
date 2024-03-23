7b519414866e5ad89b72fe5b11f252623a893a9d
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:12:06 +0100

    creacion constructor tienda

--- PracticaEntornos/src/Supermercado/Tienda.java
+++ PracticaEntornos/src/Supermercado/Tienda.java
@@ -7,7 +7,10 @@ public class Tienda {
 	private String nombre;
 	private ArrayList<Producto> productos;
 	
-	
+	public Tienda(String nombre) {
+		this.nombre = nombre;
+		this.productos = new ArrayList<Producto>();
+	}
 	
 	public String getNombre() {
 		return nombre;
