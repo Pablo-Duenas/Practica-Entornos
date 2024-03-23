2eb679644fe3204c1670d34230ad79e680281dc9
pablo-duenas <pablo.duenas1@educa.madrid.org>
2024-03-23 17:24:56 +0100

    nueva version

--- PracticaEntornos/src/Supermercado/Producto.java
+++ PracticaEntornos/src/Supermercado/Producto.java
@@ -33,24 +33,15 @@ public class Producto {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 
 	public double getPrecio() {
 		return precio;
 	}
 
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
 
 	public boolean isEnStock() {
 		return enStock;
 	}
 
-	public void setEnStock(boolean enStock) {
-		this.enStock = enStock;
-	}
 
 }
